5b7bb0511f1b57f534373ac14eeb5a0956620f69
Martijn Kooijman <martijn@fresh-dev.nl>
2024-03-07 00:09:37 +0100

    start of project

---
+++ script.js
@@ -0,0 +1,20 @@
+const submitForm1Btn = document.getElementById('submitForm1Btn').value;
+const firstNameDied = document.getElementById('firstNameDied').value;
+const secondNameDied = document.getElementById('secondNameDied').value;
+const bsnDied = document.getElementById('bsnDied').value;
+const dateOfDied = document.getElementById('dateOfDied').value;
+
+
+
+
+
+function checkForm1() {
+
+}
+
+
+
+
+
+
+submitForm1Btn.addEventListener('click', checkForm1);
